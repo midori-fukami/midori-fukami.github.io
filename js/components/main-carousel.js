a72a91cf9141c3fd4497135476ed015b6bcee7f2
Midori Fukami <midori.fukami@hotmail.com>
2024-11-01 17:32:44 +0100

    add

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,19 +1,21 @@
-const btnRight = document.querySelector('.button-arrow.-right');
-const btnLeft = document.querySelector('.button-arrow.-left');
-const elements = document.querySelector('.elements');
-const form = document.querySelector('form');
+const btnRight = document.querySelector(".button-arrow.-right");
+const btnLeft = document.querySelector(".button-arrow.-left");
+const elements = document.querySelector(".elements");
+const form = document.querySelector("form");
 let pixels = 0;
 
-btnRight.addEventListener('click', function () {
-  pixels -= 100
+btnRight.addEventListener("click", function () {
+  pixels -= 180;
+  //if(pixels < )
   elements.style = `transform: translateX(${pixels}px);`;
 });
 
-btnLeft.addEventListener('click', function () {
-  pixels += 100
+btnLeft.addEventListener("click", function () {
+  pixels += 180;
+  if (pixels > 0) pixels = 0;
   elements.style = `transform: translateX(${pixels}px);`;
 });
 
-form.addEventListener('submit', function () {
-  alert('not working yet')
+form.addEventListener("submit", function () {
+  alert("not working yet");
 });
